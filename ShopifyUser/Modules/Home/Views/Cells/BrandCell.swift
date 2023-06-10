3caff1b9b668fd069445f37ed4401301443dfa5c
Haidy <haidyyassin16@gmail.com>
2023-06-10 08:38:30 +0300

    feat: home UI: Ads and Brands UI, Brands Model

---
+++ ShopifyUser/Modules/Home/Views/Cells/BrandCell.swift
@@ -0,0 +1,16 @@
+//
+//  BrandCell.swift
+//  ShopifyUser
+//
+//  Created by Mac on 09/06/2023.
+//
+
+import UIKit
+
+class BrandCell: UICollectionViewCell {
+    
+    @IBOutlet weak var brandImage: UIImageView!
+    
+    @IBOutlet weak var brandName: UILabel!
+    
+}
---
+++ ShopifyUser/Modules/Home/Views/Cells/adsCell.swift
@@ -0,0 +1,13 @@
+//
+//  adsCell.swift
+//  ShopifyUser
+//
+//  Created by Mac on 09/06/2023.
+//
+
+import UIKit
+
+class adsCell: UICollectionViewCell {
+    
+    @IBOutlet weak var adImage: UIImageView!
+}
